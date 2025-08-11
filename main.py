f48b74c04eddae7a4695d93f5dfc54c283c946d7
Guailoudou <guailoudou@outlook.com>
2025-08-11 15:00:36 +0800

    up3

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
-
+import os
 @register("getcode", "Guailoudou", "一个简单的 GET 插件", "1.0.0")
 class MyPlugin(Star):
     def __init__(self, context: Context):
@@ -18,8 +18,8 @@ class MyPlugin(Star):
         message_str = event.message_str # 用户发的纯文本消息字符串
         message_obj = event.message_obj # 用户所发的消息的消息链 # from astrbot.api.message_components import *
         
-        logger.info(message_obj)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!  {message_obj.sender.user_id}") # 发送一条纯文本消息
+        qq = message_obj.sender.user_id
+        yield event.plain_result(f"{os.chdir}, 你发了 {message_str}!  {message_obj.sender.user_id}") # 发送一条纯文本消息
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
