288bde3638659c9be643caf43ac141fd9dfef69b
Guailoudou <guailoudou@outlook.com>
2025-08-11 14:44:55 +0800

    1

--- main.py
+++ main.py
@@ -16,10 +16,10 @@ class MyPlugin(Star):
         """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
         user_name = event.get_sender_name()
         message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
+        message_obj = event.message_obj # 用户所发的消息的消息链 # from astrbot.api.message_components import *
         
         logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}! {message_chain[0]}") # 发送一条纯文本消息
+        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}! {message_obj.message_id} - {message_obj.message}") # 发送一条纯文本消息
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
