a1eb2836a5c280ecb0880fb52e46f37d5385ca19
Guailoudou <guailoudou@outlook.com>
2025-08-23 02:50:50 +0800

    s

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ class LotteryPlugin(Star):
     # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
     @filter.command("参与抽奖")
     async def addqq(self, event: AstrMessageEvent):
-        """这是一个 get 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
+        """参加""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
         user_name = event.get_sender_name()
         # message_str = event.message_str # 用户发的纯文本消息字符串
         message_obj = event.message_obj # 用户所发的消息的消息链 # from astrbot.api.message_components import *
@@ -124,6 +124,19 @@ class LotteryPlugin(Star):
             Comp.Plain(f" \n中奖信息：\nQQ号：{info[0]}\n用户名：{info[1]}"),
             Comp.Image.fromURL("https://file.gldhn.top/img/1721313589276slitu2.png"),
         ]
+
+        if not os.path.exists("winlist.json"):
+            with open("winlist.json", "w") as f:
+                json.dump([], f)
+        with open("winlist.json", "r") as f:
+            winlist = json.load(f)
+        winlist.append({
+            "time": time.time(),
+            "info": info
+        })
+        with open("winlist.json", "w") as f:
+            json.dump(msgg, f)
+
         with open("msggroup.json", "r") as f:
             msgg = json.load(f)
         for i in msgg:
@@ -177,10 +190,18 @@ class LotteryPlugin(Star):
         chain = [
                 Comp.Plain(f"{msgg}")
             ]
-        
-        
         yield event.chain_result(chain)
         
+    @filter.permission_type(filter.PermissionType.ADMIN)
+    @filter.command("getwinlist")
+    async def getwinlist(self, event: AstrMessageEvent):
+        with open("getwinlist.json", "r") as f:
+            mgetwinlist = json.load(f)
+        chain = [
+                Comp.Plain(f"{mgetwinlist}")
+            ]
+        yield event.chain_result(chain)
+    
     @filter.permission_type(filter.PermissionType.ADMIN)
     @filter.command("取消抽奖")
     async def stop(self, event: AstrMessageEvent):
