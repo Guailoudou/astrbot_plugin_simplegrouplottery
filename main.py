4a54499113cefbdaf072e383504bfcafe037e7ad
Guailoudou <guailoudou@outlook.com>
2025-08-23 00:40:19 +0800

    12

--- main.py
+++ main.py
@@ -113,6 +113,7 @@ class LotteryPlugin(Star):
     @filter.permission_type(filter.PermissionType.ADMIN)
     @filter.command("定时抽奖")
     async def timestart(self, event: AstrMessageEvent,times: int):
+        print(times)
         global task
         task = asyncio.create_task()
         yield event.plain_result(f"已开始定时抽奖，请等待{times}秒")
