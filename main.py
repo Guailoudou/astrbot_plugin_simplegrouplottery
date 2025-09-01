406375450a8d95463abfc6162bf3d0b9527e15e3
Guailoudou <guailoudou@outlook.com>
2025-09-01 11:23:22 +0800

    更新

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
-from astrbot.api.star import Context, Star, register
+from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult,MessageChain
+from astrbot.api.star import Context, Star, register,StarTools
 from astrbot.api import logger
 import astrbot.api.message_components as Comp
 import os,json,random,time,asyncio
@@ -7,12 +7,60 @@ import os,json,random,time,asyncio
 class LotteryPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
-        self.task = None
+        self.data_dir = StarTools.get_data_dir()
+        self.data_dir.mkdir(parents=True, exist_ok=True)
+        self.qqs_file = self.data_dir / r"qqs.json"
+        self.group_file = self.data_dir / r"msggroup.json"
+        self.task_file = self.data_dir / r"task.json"
+        self.winlist_file = self.data_dir / r"winlist.json"
+        self.qqs_data = []
+        self.group_data = []
+        self.task_data = []
+        self.winlist_data = []
+        # self.task = None
 
     async def initialize(self):
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
+        await self.init()
+        await asyncio.create_task(self.tick(self))
+        logger.info("插件初始化完成")
     
-    
+    async def init(self):
+        """数据初始化"""
+        if not self.qqs_file.exists():
+            with open(self.qqs_file, "w") as f:
+                json.dump([], f)
+        if not self.group_file.exists():
+            with open(self.group_file, "w") as f:
+                json.dump([], f)
+        if not self.task_file.exists():
+            with open(self.task_file, "w") as f:
+                json.dump([], f)
+        if not self.winlist_file.exists():
+            with open(self.winlist_file, "w") as f:
+                json.dump([], f)
+        with open(self.qqs_file, "r") as f:
+            self.qqs_data = json.load(f)
+        with open(self.group_file, "r") as f:
+            self.group_data = json.load(f)
+        with open(self.task_file, "r") as f:
+            self.task_data = json.load(f)
+        with open(self.winlist_file, "r") as f:
+            self.winlist_data = json.load(f)
+    async def save(self,type:str):
+        """数据保存"""
+        if(type == "qqs"):
+            with open(self.qqs_file, "w") as f:
+                json.dump(self.qqs_data, f)
+        if(type == "group"):
+            with open(self.group_file, "w") as f:
+                json.dump(self.group_data, f)
+        if(type == "task"):
+            with open(self.task_file, "w") as f:
+                json.dump(self.task_data, f)
+        if(type == "winlist"):
+            with open(self.winlist_file, "w") as f:
+                json.dump(self.winlist_data, f)
     # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
     @filter.command("joinLottery", alias={'参与抽奖', '参加抽奖'})
     async def addqq(self, event: AstrMessageEvent):
@@ -22,54 +70,43 @@ class LotteryPlugin(Star):
         message_obj = event.message_obj # 用户所发的消息的消息链 # from astrbot.api.message_components import *
         
         qq = message_obj.sender.user_id
-        #写入json文件，文件不存在则创建
-        with open("msggroup.json", "r") as f:
-            msgg = json.load(f)
-        if event.unified_msg_origin not in msgg:
+
+        if event.unified_msg_origin not in self.group_data:
             yield event.plain_result("你没有参与抽奖的权限或不符合参与条件")
             return
-        if not os.path.exists("code.json"):
-            with open("code.json", "w") as f:
-                json.dump({}, f)
-        with open("code.json", "r") as f:
-            code = json.load(f)
-        if qq in code:
+
+        if qq in self.qqs_data:
             yield event.plain_result("你已经申请过参与抽奖了，请勿重复参与")
             return
         else:
-            code[qq] = user_name
-            with open("code.json", "w") as f:
-                json.dump(code, f)
+            self.qqs_data[qq] = user_name
+            await self.save("qqs")
         if self.task is not None:
             logger.info("当前存在正在执行的任务")
         yield event.plain_result(f"用户{message_obj.sender.user_id}参与成功") # 发送一条纯文本消息
 
     @filter.permission_type(filter.PermissionType.ADMIN)
-    @filter.command("getqqs")
+    @filter.command("lottery_getqqs")
     async def getqqs(self, event: AstrMessageEvent):
         """获取QQ列表"""
-        with open("code.json", "r") as f:
-            code = json.load(f)
-            chain = [
-                Comp.Plain(f"{code}")
-            ]
+        chain = [
+            Comp.Plain(f"{self.qqs_data}")
+        ]
         yield event.chain_result(chain)
 
     @filter.permission_type(filter.PermissionType.ADMIN)
-    @filter.command("rmqq")
+    @filter.command("lottery_rmqq")
     async def rmqq(self, event: AstrMessageEvent, qq: str):
         """删除QQ"""
-        with open("code.json", "r") as f:
-            code = json.load(f)
-            if qq not in code and qq != "all":
-                chain = [
-                    Comp.Plain("QQ不存在")
-                ]
-                yield event.chain_result(chain)
-                return
+        code = self.qqs_data
+        if qq not in code and qq != "all":
+            chain = [
+                Comp.Plain("QQ不存在")
+            ]
+            yield event.chain_result(chain)
+            return
         if qq == "all":
             code.clear()
-
             chain = [
                 Comp.Plain("已删除所有QQ")
             ]
@@ -77,18 +114,38 @@ class LotteryPlugin(Star):
         else:
             del code[qq]
             yield event.chain_result([Comp.Plain(f"删除 {qq} 成功")])
-        with open("code.json", "w") as f:
-            json.dump(code, f)
+        await self.save("qqs")
     
     @filter.permission_type(filter.PermissionType.ADMIN)
-    @filter.command("rmgroup")
+    @filter.command("lottery_rmgroup")
     async def rmgroup(self, event: AstrMessageEvent):
         """删除all开奖源群"""
-        with open("msggroup.json", "w") as f:
-            json.dump([], f)
+        self.group_data.clear()
+        await self.save("group")
         yield event.chain_result([Comp.Plain("删除成功")])
 
-    
+    async def tick(self,event: AstrMessageEvent):
+        """隔5min检查一次是否有过时间但未激活的任务"""
+        while True:
+            for i in self.task_data:
+                if not i["runned"]:
+                    if i["time"] < time.time():
+                        i["runned"] = True
+                        newtime = time.strftime('%Y%m%d%H%M%S', time.localtime(time.time()))
+                        if i["time"] < newtime:
+                            await self.Lotterystart(self)
+                            i["runned"] = True
+                            await self.save("task")
+            await asyncio.sleep(60)
+
+    @filter.permission_type(filter.PermissionType.ADMIN)
+    @filter.command("lottery_addgroup")
+    async def addgroup(self,event: AstrMessageEvent):
+        """添加开奖源群"""
+        self.group_data.append(event.unified_msg_origin)
+        await self.save("group")
+        yield event.plain_result("已设置该群组为开奖群组")
+
 
     async def timeout(self, event: AstrMessageEvent,times: int):
         try:
@@ -109,14 +166,13 @@ class LotteryPlugin(Star):
         # yield event.chain_result([Comp.Plain(f"已等待{times}秒")])
         # task = asyncio.create_task(timed_task(event,times))
     
-    @filter.permission_type(filter.PermissionType.ADMIN)
-    @filter.command("开始抽奖")
-    async def Lotterystart(self, event: AstrMessageEvent):
+    # @filter.permission_type(filter.PermissionType.ADMIN)
+    # @filter.command("开始抽奖")
+    async def Lotterystart(self):
         logger.info("开始抽奖")
         # yield event.chain_result([Comp.Plain(f"将在{times}秒")])
         # await asyncio.sleep(times)
-        with open("code.json", "r") as f:
-            code = json.load(f)
+        code = self.qqs_datas
         length = len(code)
         num = random.randint(0, length - 1)
         info = list(code.items())[num]
@@ -129,102 +185,69 @@ class LotteryPlugin(Star):
             Comp.Plain(f" \n中奖信息：\nQQ号：{info[0]}\n用户名：{info[1]}\n奖品：Minecraft: Java & Bedrock Edition for PC × 1\n请于48小时内联系管理员领取奖品，具体领取方式请查看活动信息\n活动具体规则：https://qr18.cn/BFqYTX"),
             Comp.Image.fromURL("https://file.gldhn.top/img/17559596757340600bcb4e4b8f15c81dbda43b5be19ed.png"),
         ]
-
-        if not os.path.exists("winlist.json"):
-            with open("winlist.json", "w") as f:
-                json.dump([], f)
-        with open("winlist.json", "r") as f:
-            winlist = json.load(f)
+        winlist = self.winlist_data
         winlist.append({
             "time": time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())),
             "info": info
         })
-        with open("winlist.json", "w") as f:
-            json.dump(winlist, f)
+        await self.save("winlist")
 
-        with open("msggroup.json", "r") as f:
-            msgg = json.load(f)
+        msgg = self.msggroup_data
         for i in msgg:
-            await self.context.send_message(i,event.chain_result(chain))
-        if event.unified_msg_origin not in msgg:
-            await self.context.send_message(event.unified_msg_origin,event.chain_result(chain))
-
-        # if event.get_platform_name() == "aiocqhttp":
-        # # qq
-        #     from astrbot.core.platform.sources.aiocqhttp.aiocqhttp_message_event import AiocqhttpMessageEvent
-        #     assert isinstance(event, AiocqhttpMessageEvent)
-        #     client = event.bot # 得到 client
-        #     payloads = {
-        #         "user_id": info[0],
-        #         "message": [
-        #                         {
-        #                             "type": "text",
-        #                             "data": {
-        #                                 "text": "你好"
-        #                             }
-        #                         }
-        #                     ],
-        #     }
-        #     ret = await client.api.call_action('send_private_msg', **payloads) # 调用 协议端  API
-        #     logger.info(f"delete_msg: {ret}")
-        #     winorigin = "aiocqhttp:FriendMessage:" + info[0]
-        #     if winorigin not in msgg:
-        #         await self.context.send_message(winorigin,event.chain_result(chain))
-
-        if self.task is not None:
-            self.task.cancel()
-        self.task = None
+            await self.context.send_message(i,MessageChain(chain))
         
-        event.stop_event()
 
 
     
     @filter.permission_type(filter.PermissionType.ADMIN)
     @filter.command("定时抽奖")
-    async def timestart(self, event: AstrMessageEvent,times: float):
+    async def timestart(self, event: AstrMessageEvent,times: int):
+        # times = 20250903200000
         logger.info(times)
-        times = int(times*24*60*60)
-        # LotteryPlugin.Lotterystart(self, event)
-        # task = LotteryPlugin.task
+        task = {
+            "time" : times,
+            "runned" : False
+        }
+        self.task_data.append(task)
+        await self.save("task")
         logger.info("启动定时抽奖")
-        if self.task is not None:
-            yield event.public_reply("已有定时抽奖任务，如果需要更改时间，请先取消当前任务")
+        if len(self.task_data) >= 1:
+            yield event.public_reply("已有定时抽奖任务，如果需要更改时间，请先取消当前任务，暂不支持多任务")
             return
-        logger.info("开始定时抽奖")
-        self.task = asyncio.create_task(LotteryPlugin.timeout(self, event,times))
-        # yield event.plain_result(f"已开始定时抽奖，请等待{times}秒")
+        logger.info("已加入任务")
+        times_str = str(times)
+        times_str_gsh = times_str[0:4]+"年"+times_str[4:6]+"月"+times_str[6:8]+"日"+times_str[8:10]+"时"+times_str[10:12]+"分"+times_str[12:14]+"秒"
         chain = [
-            Comp.Plain(f"已开始抽奖活动，将于{times}秒后开奖\n参与方式：发送 /参与抽奖 \n奖品：Minecraft: Java & Bedrock Edition for PC × 1\n具体领取方式请查看活动信息\n活动具体规则：https://qr18.cn/BFqYTX"),
+            Comp.Plain(f"已开始抽奖活动，将于{times_str_gsh}开奖\n参与方式：发送 /参与抽奖 \n奖品：Minecraft: Java & Bedrock Edition for PC × 1\n具体领取方式请查看活动信息\n活动具体规则：https://qr18.cn/BFqYTX"),
             Comp.Image.fromURL("https://file.gldhn.top/img/17559596757340600bcb4e4b8f15c81dbda43b5be19ed.png"),
         ]
-        with open("msggroup.json", "r") as f:
-            msgg = json.load(f)
+        msgg = self.group_data
         for i in msgg:
             await self.context.send_message(i,event.chain_result(chain))
         if event.unified_msg_origin not in msgg:
             await self.context.send_message(event.unified_msg_origin,event.chain_result(chain))
         event.stop_event()
     
-    @filter.permission_type(filter.PermissionType.ADMIN)
-    @filter.command("setmsggroup")
-    async def setmsggroup(self, event: AstrMessageEvent):
-        if not os.path.exists("msggroup.json"):
-            with open("msggroup.json", "w") as f:
-                json.dump([], f)
-        with open("msggroup.json", "r") as f:
-            msgg = json.load(f)
-        logger.info(msgg)
-        msgg.append(event.unified_msg_origin)
-        with open("msggroup.json", "w") as f:
-            json.dump(msgg, f)
+    # @filter.permission_type(filter.PermissionType.ADMIN)
+    # @filter.command("setmsggroup")
+    # async def setmsggroup(self, event: AstrMessageEvent):
+    #     if not os.path.exists("msggroup.json"):
+    #         with open("msggroup.json", "w") as f:
+    #             json.dump([], f)
+    #     with open("msggroup.json", "r") as f:
+    #         msgg = json.load(f)
+    #     logger.info(msgg)
+    #     msgg.append(event.unified_msg_origin)
+    #     with open("msggroup.json", "w") as f:
+    #         json.dump(msgg, f)
         
-        yield event.plain_result("已设置该群组为开奖群组")
+    #     yield event.plain_result("已设置该群组为开奖群组")
 
     @filter.permission_type(filter.PermissionType.ADMIN)
-    @filter.command("getmsggroup")
+    @filter.command("lottery_getmsggroup")
     async def getmsggroup(self, event: AstrMessageEvent):
-        with open("msggroup.json", "r") as f:
-            msgg = json.load(f)
+        """获取开奖群组"""
+        msgg = self.group_data
         chain = [
                 Comp.Plain(f"{msgg}")
             ]
@@ -233,8 +256,8 @@ class LotteryPlugin(Star):
     @filter.permission_type(filter.PermissionType.ADMIN)
     @filter.command("getwinlist")
     async def getwinlist(self, event: AstrMessageEvent):
-        with open("winlist.json", "r") as f:
-            winlist = json.load(f)
+        """获取中奖名单"""
+        winlist = self.winlist_data
         chain = [
                 Comp.Plain(f"{winlist}")
             ]
@@ -243,15 +266,15 @@ class LotteryPlugin(Star):
     @filter.permission_type(filter.PermissionType.ADMIN)
     @filter.command("取消抽奖")
     async def stop(self, event: AstrMessageEvent):
+        """取消抽奖"""
         # task = LotteryPlugin.task
-        if self.task is None:
+        if len(self.task_data) == 0:
             yield event.plain_result("没有进行中的抽奖")
             return
-        self.task.cancel()
-        self.task = None
+        self.task_data = []
+        await self.save("task")
         yield event.plain_result("已取消抽奖")
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
-        #删除文件
-        os.remove("code.json")
+        
