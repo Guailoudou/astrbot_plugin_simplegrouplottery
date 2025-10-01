4f794d9ded35741e09a612fa96f5bf9bfd2f4c38
Guailoudou <guailoudou@outlook.com>
2025-10-01 18:42:06 +0800

    a

--- main.py
+++ main.py
@@ -322,5 +322,5 @@ lt_getwinlist"""
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
         self.task.cancel()
-        await self.task = None
+        self.task = None
         
