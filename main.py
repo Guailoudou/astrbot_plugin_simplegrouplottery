f149e9d6470d0be3ef678222382ef8de8d809fe0
Guailoudou <guailoudou@outlook.com>
2025-10-01 18:31:04 +0800

    a

--- main.py
+++ main.py
@@ -129,7 +129,7 @@ class LotteryPlugin(Star):
         await asyncio.sleep(2)
         while True:
             for i in self.task_data.values():
-                logger.info(f"检查任务:{i}")
+                logger.info(f"检查任务:{i['name']}")
                 if not i["runned"] and i["start"]:
                     logger.info("开始判断时间")
                     newtime = time.strftime('%Y%m%d%H%M%S', time.localtime(time.time()))
